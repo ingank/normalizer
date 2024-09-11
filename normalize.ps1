997fdce8120cfb8f2c86c749aec7cadf701d9e68
Ingolf Ankert <github@ingank.net>
2024-09-11 19:49:17 +0200

    Update normalize.ps1
    zur Analyse wird nun youlean meter verwendet

--- normalize.ps1
+++ normalize.ps1
@@ -1,71 +1,44 @@
-# Variablen und Einstellungen
-$iT = -20
-
-# Aktuellen Pfad auf Skript-Ordner setzen
-Push-Location $(Split-Path $Script:MyInvocation.MyCommand.Path)
-
-# Ordner für normalisierte Dateien erstellen
-mkdir normalized 2>&1 1> $null
-
-# Sounddateien des aktuellen Ordners ermitteln
+$currentPath = (Get-Location).Path + '\'
 $soundFiles = cmd /r dir /b /on *.mp3 *.flac *.wav *.aiff
+$decodedFile = $currentPath + "decoded.wav"
+$analyzeFile = $currentPath + "decoded.txt"
 
-# einzelne Soundfiles abarbeiten
-$soundFiles | ForEach-Object {
-    $soundFilePath = $_
-    $configFilePath = $soundFilePath + ".txt"
-    $normalizedFilePath = '.\normalized\'
-    $normalizedFilePath += [System.IO.Path]::GetFileName($soundFilePath)
-    $normalizedFilePath += '.flac'
 
-    if (Test-Path "${normalizedFilePath}") {
+try {
 
-        Write-Host "Allready Done :: ${normalizedFilePath}"
+    $soundFiles | ForEach-Object {
 
-    }
-    else {
-
-        $inhalt = Get-Content $configFilePath
-
-        # Initialisiere Variablen für die Werte
-        $loudness_i = $null
-        $loudness_threshold = $null
-        $lra = $null
-        $lra_threshold = $null
-        $lra_low = $null
-        $lra_high = $null
-        $true_peak = $null
-
-        # Durchlaufe jede Zeile und extrahiere die Werte
-        foreach ($zeile in $inhalt) {
-            if ($zeile -match "Integrated Loudness \(I\):\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $loudness_i = $matches[1]
-            }
-            elseif ($zeile -match "Loudness Threshold:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $loudness_threshold = $matches[1]
-            }
-            elseif ($zeile -match "Loudness Range \(LRA\):\s+([-+]?\d+(\.\d+)?) LU") {
-                $lra = $matches[1]
-            }
-            elseif ($zeile -match "Loudness Range Threshold:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $lra_threshold = $matches[1]
-            }
-            elseif ($zeile -match "LRA Low:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $lra_low = $matches[1]
-            }
-            elseif ($zeile -match "LRA High:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $lra_high = $matches[1]
-            }
-            elseif ($zeile -match "True Peak:\s+([-+]?\d+(\.\d+)?) dBFS") {
-                $true_peak = $matches[1]
-            }
+        $soundFile = $_
+        $ebur128File = "${soundFile}.ebur128"
+    
+        if (Test-Path "${ebur128File}") {
+            Write-Host "Already analyzed :: ${soundFile}"
         }
 
-        $iDiff = $iT - (([decimal]$lra_high + [decimal]$loudness_i * 2) / 3) + ($lra / 4)
-        $iDiff = [math]::Round($iDiff, 2)
+        else {
 
-        Write-Host "Normalize :: ${normalizedFilePath} :: ${iDiff}"
+            Write-Host "Analyze :: ${soundFile}"
+            ffmpeg -hide_banner -v error -i $soundFile -y $decodedFile
+            .\ylm2.exe --input-file-path $decodedFile --export --export-type "TEXT_SUMMARY" > $null
 
-        ffmpeg -v error -i ${soundFilePath} -af volume=${iDiff}dB -ar 44100 ${normalizedFilePath}
+            $text = Get-Content $analyzeFile
+            $lastSixRows = $text[-6..-1]
+            $lastSixRows | Set-Content $ebur128File
+            Remove-Item -Path $analyzeFile -Force
+
+        }
+    }
+}
+
+finally {
+
+    Write-Host "Test $decodedFile"
+
+    if (Test-Path $analyzeFile) {
+        Remove-Item -Path $analyzeFile
+    }
+
+    if (Test-Path $decodedFile) {
+        Remove-Item -Path "$decodedFile"
     }
 }
