fe42a994c7ee927c5971dff83897f0f10e1ccd04
Ingolf Ankert <github@ingank.net>
2024-09-18 22:17:50 +0200

    Update analyze.ps1
    use the better analyzing tool "youlean loudness meter 2", now

--- analyze.ps1
+++ analyze.ps1
@@ -1,76 +1,50 @@
-# Variablen und Einstellungen
-$dateiListe = "list.txt"
-$eingabeDateiPfad = "text.txt"
+$currentPath = (Get-Location).Path + '\'
+$soundFiles = cmd /r dir /b /on *.mp3 *.flac *.wav *.aiff
+$decodedFile = $currentPath + "decoded.wav"
+$analyzeFile = $currentPath + "decoded.txt"
 
-# Aktuellen Pfad auf Skript-Ordner setzen
-Push-Location $(Split-Path $Script:MyInvocation.MyCommand.Path)
+try {
 
-# Sounddateien des aktuellen Ordners sammeln
-cmd /r dir /b /on *.mp3 *.flac *.wav *.aiff > $dateiListe
+    $soundFiles | ForEach-Object {
 
-# Alle Sounddateien des Ordners analysieren
-Get-Content -Path $dateiListe | ForEach-Object {
-    $dateiPfad = $_
-    if (Test-Path "${dateiPfad}.txt") {
-        Write-Host "Already Analyzed :: ${dateiPfad}.txt"
-    }
-    else {
+        $soundFile = $_
+        $ebur128File = "${soundFile}.ebur128"
+    
+        if (Test-Path "${ebur128File}") {
 
-        Write-Host "Analyze :: ${dateiPfad}"
+            Write-Host "Already analyzed :: ${soundFile}"
 
-        ffmpeg -hide_banner -i ${dateiPfad} -af ebur128=framelog=quiet:peak=true -f null - 2> ${eingabeDateiPfad}
-
-        # Lese den gesamten Inhalt der Datei ein
-        $inhalt = Get-Content $eingabeDateiPfad
-
-        # Initialisiere Variablen für die Werte
-        $loudness_i = $null
-        $loudness_threshold = $null
-        $lra = $null
-        $lra_threshold = $null
-        $lra_low = $null
-        $lra_high = $null
-        $true_peak = $null
-        $threshold_flag = 0
-
-        # Durchlaufe jede Zeile und extrahiere die Werte
-        foreach ($zeile in $inhalt) {
-            if ($zeile -match "I:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $loudness_i = $matches[1]
-            }
-            elseif ($zeile -match "Threshold:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                if ($threshold_flag) {
-                    $lra_threshold = $matches[1]
-                }
-                else {
-                    $loudness_threshold = $matches[1]
-                    $threshold_flag = 1
-                }
-            }
-            elseif ($zeile -match "LRA:\s+([-+]?\d+(\.\d+)?) LU") {
-                $lra = $matches[1]
-            }
-            elseif ($zeile -match "LRA low:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $lra_low = $matches[1]
-            }
-            elseif ($zeile -match "LRA high:\s+([-+]?\d+(\.\d+)?) LUFS") {
-                $lra_high = $matches[1]
-            }
-            elseif ($zeile -match "Peak:\s+([-+]?\d+(\.\d+)?) dBFS") {
-                $true_peak = $matches[1]
-            }
         }
 
-        # Ausgabe der extrahierten Werte
-        $text = @(
-            "Integrated Loudness (I): $loudness_i LUFS",
-            "Loudness Threshold: $loudness_threshold LUFS",
-            "Loudness Range (LRA): $lra LU",
-            "Loudness Range Threshold: $lra_threshold LUFS",
-            "LRA Low: $lra_low LUFS",
-            "LRA High: $lra_high LUFS",
-            "True Peak: $true_peak dBFS"
-        )
-        $text | Out-File -FilePath "${dateiPfad}.txt"
+        else {
+
+            Write-Host "Analyze :: ${soundFile}"
+
+            ffmpeg -hide_banner -v error -i $soundFile -y $decodedFile
+            .\ylm2.exe --input-file-path $decodedFile --export --export-type "TEXT_SUMMARY" > $null
+
+            $text = Get-Content $analyzeFile
+            $lastSixRows = $text[-6..-1]
+            $lastSixRows | Set-Content $ebur128File
+            Remove-Item -Path $analyzeFile -Force
+
+        }
+    }
+}
+
+finally {
+
+    Write-Host "Leave and clean up ..."
+
+    if (Test-Path $analyzeFile) {
+
+        Remove-Item -Path $analyzeFile
+
+    }
+
+    if (Test-Path $decodedFile) {
+
+        Remove-Item -Path "$decodedFile"
+
     }
 }
